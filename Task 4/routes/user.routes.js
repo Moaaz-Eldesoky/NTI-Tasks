82b870e4e79df33e2f866a3e7ba2dafdbbf0735f
Moaaz Eldesoky <moaaz.salah2014@gmail.com>
2022-06-25 13:08:17 +0200

    First Angular Task compleated.

---
+++ Task 4/routes/user.routes.js
@@ -0,0 +1,18 @@
+const router = require("express").Router()
+const userController = require("../controllers/user.controller")
+
+//user Routes
+router.get("/", userController.homeUser)
+router.get("/singleUser/:id", userController.singleUser)
+router.get("/addPostUser", userController.addPostUser)
+router.post("/addPostUser", userController.addPostLogicUser)
+router.get("/editUser/:id", userController.editUser)
+router.post("/editUser/:id", userController.editLogicUser)
+router.get("/deleteUser/:id", userController.deleteUser)
+router.get("/editStatus/:id", userController.editStatus)
+router.post("/editStatus/:id", userController.editLogicStatus)
+
+
+
+module.exports = router
+
